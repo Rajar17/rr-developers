99fad795743db452a0d8cf1f84c971a2dc2a5b38
Raja R <93196309+Rajar17@users.noreply.github.com>
2025-05-31 14:47:58 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,31 +1,24 @@
 export interface Property {
-       id: string;
-       title: string;
-       type: string;
-       price: number;
-       area: number;
-       areaUnit: string;
-       location: string;
-       facing: string;
-       description: string;
-       features: string[];
-       image: string;
-       isFeatured?: boolean;
-       amenities?: string[];
-       parking?: string;
-       proximity?: {
-         hospitals: string;
-         restaurants: string;
-         parks: string;
-         airport: string;
-         busStand: string;
-         railwayStation: string;
-       };
-     }
-
-     export interface Brochure {
-       id: string;
-       title: string;
-       description: string;
-       thumbnail: string;
-     }
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  area: number;
+  areaUnit: string;
+  location: string;
+  facing: string;
+  description: string;
+  features: string[];
+  image: string;
+  isFeatured?: boolean;
+  amenities?: string[];
+  parking?: string;
+  proximity?: {
+    hospitals: string;
+    restaurants: string;
+    parks: string;
+    airport: string;
+    busStand: string;
+    railwayStation: string;
+  };
+}
