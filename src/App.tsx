0a0695ca216442c467bb559bf51f0499b7d3bd50
Raja R <93196309+Rajar17@users.noreply.github.com>
2025-06-01 22:54:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import AboutUs from './components/AboutUs';
 import Properties from './components/Properties';
-import FeaturedProject from './components/FeaturedProject';
-import Brochures from './components/Brochures';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
-import PropertyDetails from './components/PropertyDetails';
-import AllProperties from './components/AllProperties';
 import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
@@ -46,32 +41,17 @@ function App() {
 
   return (
     <AuthProvider>
-      <Router>
-        <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100">
-          <Header />
-          <main className="pt-[100px] md:pt-[80px]">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <>
-                    <Hero />
-                    <AboutUs />
-                    <Properties />
-                    <FeaturedProject />
-                    <Brochures />
-                    <ContactUs />
-                    <Footer />
-                  </>
-                }
-              />
-              <Route path="/property/:id" element={<PropertyDetails />} />
-              <Route path="/all-properties" element={<AllProperties />} />
-            </Routes>
-            <Toaster position="top-right" />
-          </main>
-        </div>
-      </Router>
+      <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100">
+        <Header />
+        <main className="pt-[100px] md:pt-[80px]">
+          <Hero id="home" />
+          <AboutUs id="about-us" />
+          <Properties id="properties" />
+          <ContactUs id="contact" />
+          <Footer />
+          <Toaster position="top-right" />
+        </main>
+      </div>
     </AuthProvider>
   );
 }
